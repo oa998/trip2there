9f8bd849907a728331b5878bc4b21d556e85fe59
zzz sss <onlineaccounts998@gmail.com>
2024-01-19 20:30:27 -0500

    auto commit

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,5 +1,5 @@
 export const isLoggedIn = () => {
-	return fetch('https://uber2u-server-glovbogi2a-uc.a.run.app/auth/ping').then(async (r) => {
+	return fetch('/data/auth/ping').then(async (r) => {
 		if (r.status == 401) {
 			const json = await r.json();
 			await new Promise((r) => setTimeout(r, 2000));
