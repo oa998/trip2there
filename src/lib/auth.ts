699071c10b5ec05a708c290c6835f8a92e094a12
zzz sss <onlineaccounts998@gmail.com>
2024-01-19 21:18:39 -0500

    auto commit

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,9 +1,24 @@
+import { goto } from '$app/navigation';
+import { toastMsg } from './toast';
+
+export const signout = () => {
+	return fetch('/data/auth/sign-out');
+};
+
 export const isLoggedIn = () => {
 	return fetch('/data/auth/ping').then(async (r) => {
 		if (r.status == 401) {
+			await signout();
 			const json = await r.json();
 			await new Promise((r) => setTimeout(r, 2000));
 			throw new Error(json.message);
 		}
+	});
+};
+
+export const signoutAndHome = () => {
+	signout().then(() => {
+		toastMsg('Logged out');
+		goto('/');
 	});
 };
--- src/routes/(no-auth)/+page.svelte
+++ src/routes/(no-auth)/+page.svelte
@@ -5,8 +5,12 @@
 
 <Center>
 	<div class="text-center">
-		<div class="title">Rides 2U</div>
-		<div class="subtitle">We'll get you there</div>
+		<div class="flex flex-row w-full justify-center gap-1 items-center">
+			<div class="title josefin font-bold">Trip</div>
+			<div class="title josefin bigger shadowz">2</div>
+			<div class="title josefin font-bold">There</div>
+		</div>
+		<div class="subtitle">One App, Endless Trips</div>
 		<img src={`${base == '/' ? '' : base}/car.webp`} alt="car" class="masked translate-up" />
 
 		<div class="flex flex-row justify-around translate-up">
@@ -17,10 +21,29 @@
 </Center>
 
 <style lang="postcss">
+	@font-face {
+		font-family: 'anybody';
+		src: url('/anybody.ttf') format('truetype-variations');
+		font-weight: 1 999;
+	}
+
+	.josefin {
+		font-family: anybody, serif;
+		font-optical-sizing: auto;
+		font-style: normal;
+	}
+
 	.title {
-		@apply text-5xl font-bold relative z-10;
+		@apply text-5xl relative z-10;
 	}
 	.subtitle {
 		@apply text-lg relative z-10;
+	}
+	.bigger {
+		font-size: 5rem;
+	}
+	.shadowz {
+		text-shadow: 0 0 10px #000090;
+		background: transparent;
 	}
 </style>
