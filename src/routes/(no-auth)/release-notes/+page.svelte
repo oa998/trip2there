ead8d31b84491d42b2ffa737cb6ff443d48e56ec
zzz sss <onlineaccounts998@gmail.com>
2024-02-27 08:41:40 -0500

    auto commit

--- src/routes/(no-auth)/release-notes/+page.svelte
+++ src/routes/(no-auth)/release-notes/+page.svelte
@@ -1,6 +1,25 @@
-<div class="flex flex-col gap-3 p-3">
+<div class="flex flex-col gap-10 p-2">
 	<div class="flex flex-col">
-		<div>Feb 26, 2024</div>
+		<div class="date">Feb 26, 2024</div>
+		<ul>
+			<header>Added phone verification</header>
+			<div>
+				<li>Phone verification via text message and UI input has been added</li>
+				<li>If a user modifies their phone number, this will un-verify their phone number. (db)</li>
+				<li>
+					If any verification is in progress (A verification code has already been sent), that
+					verification will be cancelled. (db)
+				</li>
+				<li>
+					If a user attempts phone verification but their phone number is already verified, they
+					will be redirected to their profile page and their phone will be marked as verified.
+				</li>
+			</div>
+		</ul>
+	</div>
+
+	<!-- <div class="flex flex-col">
+		<div class="date">Feb 26, 2024</div>
 		<ul>
 			<li>If a user modifies their phone number, this will un-verify their phone number.</li>
 			<li>
@@ -13,5 +32,22 @@
 				and their phone will be marked as verified.
 			</li>
 		</ul>
-	</div>
+	</div> -->
 </div>
+
+<style lang="postcss">
+	.date {
+		@apply font-bold text-center py-2;
+	}
+	ul {
+		@apply flex flex-col gap-2;
+	}
+	ul > header {
+		@apply underline;
+	}
+	ul > div > li {
+		list-style: circle;
+		list-style-position: outside;
+		@apply ml-5;
+	}
+</style>
